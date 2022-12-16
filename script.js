0c4aa24db85517299a9a4558cccb9164b1519b9f
Naveen Doddi <109824438+NaveenDoddi@users.noreply.github.com>
2022-12-16 14:25:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -356,9 +356,4 @@ function searching(){
         }
     }
     
-    var body = document.body,
-    html = document.documentElement;
-
-    var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
-    console.log(height)
 }
